4ace0e7a1f6cbb107c4285e7895d60bb60a6a858
Felice Caricati <felice.caricati@openmindonline.it>
2019-03-07 15:15:49 +0100

    es6 class

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
-HTMLFormElement.prototype.serialize = function() {
-  const s = [];
-  let field, l;
-  if (typeof this === 'object' && this.nodeName === 'FORM') {
-    for (var i = 0; i < this.length; i++) {
-      const formElement = this[i];
-      if (
-        formElement.name &&
-        !formElement.disabled &&
-        formElement.type !== 'button' &&
-        formElement.type !== 'file' &&
-        formElement.type !== 'reset' &&
-        formElement.type !== 'submit'
-      ) {
-        if (formElement.type === 'select-multiple') {
-          l = formElement.options.length;
-          formElement.options.forEach(opt => {
-            if (opt.selected) s[s.length] = `${encodeURIComponent(formElement.name)}=${encodeURIComponent(opt.value)}`;
-          });
-        } else if ((formElement.type !== 'checkbox' && formElement.type !== 'radio') || formElement.checked) {
-          s[s.length] = `${encodeURIComponent(formElement.name)}=${encodeURIComponent(formElement.value)}`;
-        }
-      }
+export default class SerializeForm {
+  static get BLACK_LIST() {
+    return {
+      elements: ['button', 'file', 'reset', 'submit']
+    };
+  }
+
+  constructor(form) {
+    this.form = SerializeForm.serialize(form);
+  }
+
+  getData(dataType) {
+    switch (dataType) {
+      case 'array':
+        return SerializeForm.getFormDataArray(this.form);
+      case 'object':
+        return SerializeForm.getFormDataObject(this.form);
+      default:
+        return this.form.join('&').replace(/%20/g, '+');
     }
   }
-  return s.join('&').replace(/%20/g, '+');
-};
+
+  static getFormDataArray(form) {
+    return [...form.map(data => {
+      const tempData = data.split('=');
+      return {
+        name: tempData[0],
+        value: tempData[1]
+      };
+    })];
+  }
+
+  static getFormDataObject(form) {
+    const formData = {};
+    form.forEach(data => {
+      const tempData = data.split('=');
+      formData[tempData[0]] = tempData[1];
+    });
+
+    return formData;
+  }
+
+  static isValidForm(form) {
+    return typeof form === 'object' && form.nodeName === 'FORM';
+  }
+
+  static isvalidElement(el) {
+    return el.name && !el.disabled && !SerializeForm.BLACK_LIST.elements.includes(el.type);
+  }
+
+  static serialize(form) {
+    const data = [];
+    if (SerializeForm.isValidForm(form)) {
+      [...form.elements].forEach(el => {
+        if (SerializeForm.isvalidElement(el)) {
+          if ((el.type !== 'checkbox' && el.type !== 'radio') || el.checked) {
+            data.push(`${el.name}=${el.value}`);
+          } else if (el.type === 'select-multiple') {
+            el.options.forEach(opt => {
+              if (opt.selected) data.push(`${el.name}=${opt.value}`);
+            });
+          }
+        }
+      });
+    }
+    return data;
+  }
+}
